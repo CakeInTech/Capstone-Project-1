2b7612e041a4318bcd770facf3d63a5cf7ad1f83
Mohamed Abdulhakim <69153321+MohamedCK@users.noreply.github.com>
2022-07-26 22:35:24 +0200

    Added a nav menu that is not functional

---
+++ main.js
@@ -0,0 +1,6 @@
+const active = document.querySelector('.menu-cn')
+const close = document.querySelector('.link')
+
+active.addEventListener('click', () => {
+    active.classList.add('.active');
+})

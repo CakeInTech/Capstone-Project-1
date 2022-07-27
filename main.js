5c9844df3f6b13dbfeeae12403770f51c4e8e560
Mohamed Abdulhakim <69153321+MohamedCK@users.noreply.github.com>
2022-07-27 10:04:01 +0200

    main commit

--- main.js
+++ main.js
@@ -1,6 +1,16 @@
-const active = document.querySelector('.menu-cn')
+const active = document.querySelector('.ham-menu')
 const close = document.querySelector('.link')
+const menu = document.querySelector('.menu-cn')
 
 active.addEventListener('click', () => {
-    active.classList.add('.active');
+    menu.style.display = 'flex';
+    active.style.display = 'none';
 })
+
+close.addEventListener('click', () => {
+    menu.style.display = 'none';
+    active.style.display = 'block'
+})
+
+
+let thirdPage = document.getElementById('.thirdpage')
